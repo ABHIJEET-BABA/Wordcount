b0dce6de5ba2672a33b6ae3d42eb083e1ce03a68
ABHIJEET-BABA <82521968+ABHIJEET-BABA@users.noreply.github.com>
2023-04-16 22:32:21 +0530

    Add files via upload

---
+++ wcount.js
@@ -0,0 +1,13 @@
+const textareaE1= document.getElementById("textarea")
+const totalcounterE1=document.getElementById("total-counter")
+const remainingcounterE1=document.getElementById("remaining-counter")
+textareaE1.addEventListener("keyup",()=>{
+    updatecounter()
+})
+
+function updatecounter(){
+    totalcounterE1.innerText=textareaE1.value.length
+    remainingcounterE1.innerText=textareaE1.getAttribute("maxlength")-textareaE1.value.length
+
+}
+
